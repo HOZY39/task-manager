e93ea7614c64e4459d2d72e0c6282a58c3e45c66
HOZY39 <igor05061@gmail.com>
2024-12-08 19:04:01 +0100

    SolRepo

---
+++ src/main/java/com/simon/task_manager/solution/SolutionRepository.java
@@ -0,0 +1,71 @@
+package com.simon.task_manager.solution;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.simon.task_manager.task.Solution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import jakarta.annotation.PostConstruct;
+
+
+
+@Repository
+public class SolutionRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(SolutionRepository.class);
+
+    private final JdbcClient jdbcClient;
+
+    public SolutionRepository(JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Solution> findAll() {
+        return jdbcClient.sql("select * from solutions")
+                .query(Solution.class)
+                .list();
+    }
+
+    public Optional<Solution> findById(Integer id) {
+        return jdbcClient.sql("SELECT * FROM solutions WHERE id = :id" )
+                .param("id", id)
+                .query(Solution.class)
+                .optional();
+    }
+
+    public void create(Solution solution) {
+        var updated = jdbcClient.sql("INSERT INTO solutions(task_id,solution) values(?,?)")
+                .params(List.of(solution.task_id().toString(),solution.solution()))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create task");
+    }
+
+//    public void update(Solution task, Integer id){
+//        var updated = jdbcClient.sql("update tasks set subject = ?, section = ?, description = ? where id = ?")
+//                .params(List.of(task.subject().toString(), task.section(), task.description(), id))
+//                .update();
+//        Assert.state(updated==1, "Failed to update task");
+//    }
+
+    public void delete(Integer id){
+        var deleted = jdbcClient.sql("delete from solutions where id = :id")
+                .param("id", id)
+                .update();
+    }
+
+    public int count() {
+        return jdbcClient.sql("select * from solutions").query().listOfRows().size();
+    }
+
+    public void saveAll(List<Solution> tasks) {
+        tasks.stream().forEach(this::create);
+    }
+}
--- src/test/java/com/simon/task_manager/TaskManagerApplicationTests.java
+++ src/test/java/com/simon/task_manager/TaskManagerApplicationTests.java
@@ -1,13 +1,15 @@
 package com.simon.task_manager;
 
+import com.simon.task_manager.task.TaskRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//
+//	}
 
 }
